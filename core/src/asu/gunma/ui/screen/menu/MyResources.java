883cbe07209b000f8e4f8591b35a7fba6fec7762
sjodon <sydney.jodon@gmail.com>
2019-11-05 21:47:17 -0700

    Add japanese

--- core/src/asu/gunma/ui/screen/menu/MyResources.java
+++ core/src/asu/gunma/ui/screen/menu/MyResources.java
@@ -5,7 +5,33 @@ import java.util.ListResourceBundle;
 public class MyResources extends ListResourceBundle {
     protected Object[][] getContents() {
         return new Object[][] {
-                {"VideoTutorials", "Video Tutorials"},
+            {"VideoTutorials", "Video Tutorials"},
+            {"EnterStudentId", "Enter Student ID."},
+            {"VideoTutorials", "Video Tutorials"},
+            {"Flashcards", "Flashcards"},
+            {"LoadNewClass", "Load New Class"},
+            {"DownloadClassData", "Download Class Data"},
+            {"Start", "Start"},
+            {"Settings", "Settings"},
+            {"QuitGame", "Quit Game?"},
+            {"Yes", "Yes"},
+            {"No", "No"},
+            {"VocabularySets", "Vocabulary Sets"},
+            {"SelectVocabularySets", "Select vocabulary sets available for gameplay."},
+            {"Numbers", "Numbers"},
+            {"ColorsShapes", "Colors/Shapes"},
+            {"Countries", "Countries"},
+            {"DaysMonths", "Days/Months"},
+            {"Feelings", "Feelings"},
+            {"Subjects", "Subjects"},
+            {"FruitsFoods", "Fruits/Foods"},
+            {"Professions", "Professions"},
+            {"Places", "Places"},
+            {"Time", "Time"},
+            {"AddVocabularySet", "Add vocabulary set."},
+            {"VocabularySetName", "Vocabulary Set Name"},
+            {"Cancel", "Cancel"},
+            {"Done", "Done"},
         };
     }
 }
---
+++ core/src/asu/gunma/ui/screen/menu/MyResources_jp.java
@@ -0,0 +1,34 @@
+package asu.gunma.ui.screen.menu;
+
+public class MyResources_jp extends MyResources{
+    protected Object[][] getContents() {
+        return new Object[][] {
+            {"EnterStudentId", "生徒番号を記入してください。"},
+            {"VideoTutorials", "発音練習のビデオ "},
+            {"Flashcards", "フラッシュカード "},
+            {"LoadNewClass", "名簿を読み込む "},
+            {"DownloadClassData", "クラスデータを書き出す "},
+            {"Start", "スタート "},
+            {"Settings", "設定 "},
+            {"QuitGame", "ゲームをやめますか。"},
+            {"Yes", "はい "},
+            {"No", "いいえ "},
+            {"VocabularySets", "単語セット "},
+            {"SelectVocabularySets", "単語セットを選択してください。"},
+            {"Numbers", "数字 "},
+            {"ColorsShapes", "色・形"},
+            {"Countries", "国 "},
+            {"DaysMonths", "曜日・月"},
+            {"Feelings", "気持ち "},
+            {"Subjects", "教科 "},
+            {"FruitsFoods", "フルーツ・食べ物"},
+            {"Professions", "仕事 "},
+            {"Places", "建物 "},
+            {"Time", "時間 "},
+            {"AddVocabularySet", "単語セットを追加する。"},
+            {"VocabularySetName", "単語セット名を記入してください。"},
+            {"Cancel", "キャンセル "},
+            {"Done", "終了 "},
+        };
+    }
+}
